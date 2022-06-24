3abb70331a77d659f09453cd1157d038e46d9080
Jo Colina <jo.colina@weezevent.com>
2022-06-24 17:17:14 +0200

    Commits: parse commits to expose message & body separately

--- src/lib/pull-request.js
+++ src/lib/pull-request.js
@@ -9,8 +9,26 @@ class PullRequest {
 		return this.pull_request.number;
 	}
 
+	parse_commits(commits) {
+		return commits.map(commit => {
+			const { message } = commit.commit;
+			const line_jump = message.indexOf('\n');
+
+			commit.message = message;
+			commit.body = null;
+			if(line_jump === -1) {
+				// We only have a single line message
+				return commit;
+			}
+
+			commit.message = message.substring(0, line_jump);
+			commit.body = message.substring(line_jump + 1);
+			return commit;
+		});
+	}
+
 	get_commits(repo) {
-		return GitHub.get(`/repos/${repo}/pulls/${this.number}/commits`);
+		return GitHub.get(`/repos/${repo}/pulls/${this.number}/commits`).then(commits => this.parse_commits(commits));
 	}
 }
 
